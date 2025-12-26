accf0d4c1a8ff705662fa7b5a0d8ec9f4a99d34a
Dhw20020726 <100478651+Dhw20020726@users.noreply.github.com>
2025-12-27 02:36:54 +0800

    Remove AnchorAuto logic and config

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
 import AnchorJump from '../components/AnchorJump';
-// AnchorAuto 会在 remark 阶段被替换为 AnchorJump，此处注册一个空壳避免意外渲染时出错
-function AnchorAuto() {
-  return null;
-}
 
 export default {
   ...MDXComponents,
   AnchorJump,
-  AnchorAuto,
 };
